682777885abe9086abe8174ff45093a5c0ece857
David Zhang <davidhqr@gmail.com>
2018-09-15 12:25:31 -0400

    add constants

---
+++ app/src/main/java/com/htn/smartspend/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.htn.smartspend.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+    public static Map<String, String> nameCustomerIDMap = new HashMap<>();
+    static {
+        nameCustomerIDMap.put("yuriaaderg", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_be23d561-0198-4876-9d23-2a5e67bad5ff");
+        nameCustomerIDMap.put("glennisabad", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_c819a4e7-0ac2-4134-ae13-5ac24abad10b");
+        nameCustomerIDMap.put("tobinaakre", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_64bb7caf-1faf-4c66-8df4-4fc7156efad2");
+    }
+}
