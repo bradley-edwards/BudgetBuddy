8fe463ebbae0467c17226793191f1ed70e1ffea9
David Zhang <davidhqr@gmail.com>
2018-09-15 14:27:21 -0400

    Integrate davinci

--- app/src/main/java/com/htn/smartspend/LoginActivity.java
+++ app/src/main/java/com/htn/smartspend/LoginActivity.java
@@ -1,28 +1,85 @@
 package com.htn.smartspend;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-    private Button button;
+import com.htn.smartspend.utils.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loginButton;
+    private EditText usernameEdit;
+
+    private SharedPreferences prefs = getSharedPreferences("info", MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button = (Button) findViewById(R.id.login_button_login);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMainActivity();
-            }
-        });
+        loginButton = findViewById(R.id.login_button_login);
+        usernameEdit = findViewById(R.id.login_edit_username);
+
+        loginButton.setOnClickListener(this);
     }
-    public void openMainActivity() {
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button_login:
+                String username = usernameEdit.getText().toString();
+                if (username.isEmpty()) {
+                    Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT);
+                } else {
+                    try {
+                        getCustomerInfo(username);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    openMainActivity();
+                }
+                break;
+        }
+    }
+
+    private void getCustomerInfo(String username) throws IOException {
+
+        String customerId = Constants.NAME_CUSTOMERID_MAP.get(username);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(Constants.BASE_URL + "/customers/" + customerId)
+                .addHeader("Authorization", Constants.API_KEY)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String result = response.body().string();
+        try {
+            JSONObject json = new JSONObject(result);
+
+            prefs.edit().putString("givenName", json.getString("givenName")).apply();
+            prefs.edit().putString("maidenName", json.getString("maidenName")).apply();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/htn/smartspend/utils/Constants.java
+++ app/src/main/java/com/htn/smartspend/utils/Constants.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 public class Constants {
 
-    public static Map<String, String> nameCustomerIDMap = new HashMap<>();
+    public static Map<String, String> NAME_CUSTOMERID_MAP = new HashMap<>();
     static {
-        nameCustomerIDMap.put("yuriaaderg", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_be23d561-0198-4876-9d23-2a5e67bad5ff");
-        nameCustomerIDMap.put("glennisabad", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_c819a4e7-0ac2-4134-ae13-5ac24abad10b");
-        nameCustomerIDMap.put("tobinaakre", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_64bb7caf-1faf-4c66-8df4-4fc7156efad2");
+        NAME_CUSTOMERID_MAP.put("yuriaaderg", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_be23d561-0198-4876-9d23-2a5e67bad5ff");
+        NAME_CUSTOMERID_MAP.put("glennisabad", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_c819a4e7-0ac2-4134-ae13-5ac24abad10b");
+        NAME_CUSTOMERID_MAP.put("tobinaakre", "5b4baf1d-a1b3-4a49-958a-4f8fbbc26e66_64bb7caf-1faf-4c66-8df4-4fc7156efad2");
     }
+
+    public static String BASE_URL = "https://api.td-davinci.com/api";
+    public static String API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiNzVjMjg3MzYtMGE2YS0zZmM4LWFlYzQtZWYxYWI4ZGU1MDNjIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI1YjRiYWYxZC1hMWIzLTRhNDktOTU4YS00ZjhmYmJjMjZlNjYifQ.l3mWgDuecKc7vtrFCOq3PDFzdnAjR0j6G1a_axsHD3o";
 }
