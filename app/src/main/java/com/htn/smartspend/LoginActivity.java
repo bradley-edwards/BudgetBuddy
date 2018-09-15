3bca3de8504800743e17b1da0ffd33cb486e6e87
brad-edwards <brad.edwardsss@gmail.com>
2018-09-15 12:22:11 -0400

    button functionality

---
+++ app/src/main/java/com/htn/smartspend/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.htn.smartspend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.login_button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
