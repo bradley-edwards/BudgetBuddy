af53b056fda710f3100c6af0063ef03f4753c058
David Zhang <davidhqr@gmail.com>
2018-09-16 00:30:32 -0400

    Change progress icons

--- app/src/main/java/com/htn/budgetbuddy/HomeActivity.java
+++ app/src/main/java/com/htn/budgetbuddy/HomeActivity.java
@@ -5,15 +5,22 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.htn.budgetbuddy.utils.TinyDB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView nameText;
     private ImageButton suggestionsImageButton, settingsImageButton, transactionsImageButton;
+    private ListView progressList;
 
     private TinyDB tinyDB;
 
@@ -28,8 +35,19 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         suggestionsImageButton = findViewById(R.id.home_imageButton_suggestions);
         settingsImageButton = findViewById(R.id.home_imageButton_settings);
         transactionsImageButton = findViewById(R.id.home_imageButton_transactions);
+        progressList = findViewById(R.id.home_list_progress);
 
-        nameText.setText("Welcome, " + tinyDB.getString("givenName"));
+        List<String> list = new ArrayList<String>();
+        list.add("Food");
+        list.add("Shopping");
+        list.add("Entertainment");
+        list.add("Transportation");
+
+        ProgressAdapter progressAdapter = new ProgressAdapter(this, list);
+
+        progressList.setAdapter(progressAdapter);
+
+        nameText.setText("Welcome, " + tinyDB.getString("givenName") + "!");
 
         suggestionsImageButton.setOnClickListener(this);
         settingsImageButton.setOnClickListener(this);
